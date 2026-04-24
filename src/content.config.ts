37206f2dd96367fdf7611b867a8695d46f2f5420
Adam Noir <webnoircode@gmail.com>
2026-04-24 17:04:24 -0600

    projects from json collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
@@ -12,6 +12,30 @@ const blog = defineCollection({
     })
 });
 
+const project = defineCollection({
+    loader: file("src/content/projects.json"),
+    schema: () => z.object({
+        id: z.string(),
+        name: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
+        liveUrl: z.string(),
+        sourceCodeUrl: z.string()
+    })
+})
+
+const socialMedia = defineCollection({
+    loader: file("src/content/socialmedia.json"),
+    schema: () => z.object({
+        id: z.string(),
+        icon: z.string(),
+        url: z.string(),
+        label: z.string()
+    })
+ })
+
 export const collections = {
-    blog
+    blog, 
+    project,
+    socialMedia
 }
